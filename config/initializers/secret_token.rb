d34ecba2036b7a4b7a66f7a14241b1f1b1e5dd4f
Jeff Deville <jeffdeville@gmail.com>
2013-01-30 10:03:03 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soapbox::Application.config.secret_token = 'de5c95f69064e327b868a9c2f31c613cd573dd4b64bdc291dd9db612955828ea6c9e96f46c55293887d2a2b230e70bb51b8a72aefbbccd79ca21bbfd74cb7906'
